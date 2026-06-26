3a08b6436596a9bc7e502a1534e9942fa4ba6132
Ratul-sh <hamim2117026@stud.kuet.ac.bd>
2026-06-26 17:16:34 +0600

    The updated file contain both brute force logic and A single loop logic using a dictionary. why the 2nd logic is better beacuse it reduces the computational time drastically.

--- two_sum.py
+++ two_sum.py
@@ -1,4 +1,4 @@
-class Solution:
+class Solution_bruteForce:
     def twoSum(self, nums, target):
         pairs = []
         for i in range(len(nums)):
@@ -8,13 +8,29 @@ class Solution:
                     pairs.append(paired_value)
         return pairs
     def print_result(self, nums_result, target_1):
-        print(Solution.twoSum(self, nums_result, target_1))
+        print(Solution_bruteForce.twoSum(self, nums_result, target_1))
     
 
- 
+class Solution_withDict:
+    def twoSum(self, nums, target):
+        storage = {}
+        for index, value in enumerate(nums):
+            complementory_value = target - value
+            if complementory_value in storage:
+                return [storage[complementory_value], index]
+            else:
+                storage[value] = index
+        
+    
+    def show_result(self, nums_show, target_show):
+        print(Solution_withDict.twoSum(self, nums_show, target_show))
 
             
-x = Solution()
+
+
+            
+x = Solution_bruteForce()
+y = Solution_withDict()
 
 # nums = [1, 2, 3, 4, 5, 76, 7]
 nums_1 = [3, 3, 3, 3, 3]
@@ -22,6 +38,6 @@ nums_1 = [3, 3, 3, 3, 3]
 target = 6
 
 # x.twoSum(nums, target)
-x.print_result(nums_1, target)
+y.show_result(nums_1, target)
 
 
