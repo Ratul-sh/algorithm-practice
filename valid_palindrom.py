d14fd3f34c502b54c5aba39efc6924276ad8473e
Ratul-sh <hamim2117026@stud.kuet.ac.bd>
2026-06-29 23:05:03 +0600

    The 2nd class actually see if a mesasge is palindrom or not without using extra memomry from the computer.

--- valid_palindrom.py
+++ valid_palindrom.py
@@ -1,4 +1,4 @@
-class Palindrom:
+class Palindrom_with_Exxtra_Memory:
     def valid_palindrom(self, message):
         x = []
         y = []
@@ -18,11 +18,33 @@ class Palindrom:
 
     
 
+class Palindrom:
+    def valid_palindrom(self, message):
+        left = 0
+        right = len(message) - 1
+        while left <= right:
+            if message[left].isalnum() == False:
+                left += 1
+            elif message[right].isalnum() == False:
+                right -= 1
+            elif message[left].lower() == message[right].lower():
+                left += 1
+                right -= 1
+            else:
+                return False
+            
+        return True
 
-x = Palindrom()
+
+
+
+
+x = Palindrom_with_Exxtra_Memory()
+y = Palindrom()
 string = "A man, A Plan, A CAnal: Panama"
 
-print(x.valid_palindrom(string))
+# print(x.valid_palindrom(string))
+print(y.valid_palindrom(string))
 
         
 
